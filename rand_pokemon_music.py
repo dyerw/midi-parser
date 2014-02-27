9a6f0c04e9a097227c3a50b6194e8c2586b71113
dyerw <dyer.w@husky.neu.edu>
2014-02-26 23:24:48 -0600

    added functionality to define instruments as hex values on command line

--- rand_pokemon_music.py
+++ rand_pokemon_music.py
@@ -1,12 +1,21 @@
 import os
 import random
+import sys
+
+generate_randomly = True
+if len(sys.argv) == 3:
+    sound1 = sys.argv[1]
+    sound2 = sys.argv[2]
+    generate_randomly = False
 
 x = open(os.path.join('test_midis', 'pokemon-center.mid')).read()
 
 hex_list = [val.encode('hex') for val in x]
 
-sound1 = hex(random.choice(range(127)))[2:]
-sound2 = hex(random.choice(range(127)))[2:]
+if generate_randomly:
+    sound1 = hex(random.choice(range(127)))[2:]
+    sound2 = hex(random.choice(range(127)))[2:]
+
 if len(sound1) == 1:
     sound1 = "0" + sound1
 if len(sound2) == 1:
